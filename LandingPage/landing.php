9ccb8fb6f403aa690c25fda287ad5b4659fc00af
Alex Perez <alexprzf@gmail.com>
2020-10-28 11:19:12 +0100

    Landing page actualització
    Fons de pantalla amb imatge i títol sobre imatge

--- LandingPage/landing.php
+++ LandingPage/landing.php
@@ -4,12 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cantina Pedralbes</title>
+    <link rel="stylesheet" href="landing.css">
 </head>
 <body>
     <header>
       <?php require_once("../General/header.php"); ?>
     </header>
-    <h1> Cantina Pedralbes </h1>
+    <div class="Background">
+      <h1 id="Titol"> Cantina Pedralbes </h1>
+    </div>
     <footer>
       <?php require_once("../General/footer.php"); ?>
     </footer>
