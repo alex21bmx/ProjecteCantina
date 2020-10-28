8fbb3f06143f2d95b425057f79bec7d8ff12af04
Alex Perez <alexprzf@gmail.com>
2020-10-28 13:18:01 +0100

    Modificació Footer i afegit OpenSans.ttf
    He donat forma al footer i ja té els elements, el css esta tot fet menys els colors. Falta que el href de Avis legal redirigeixi al lloc que toca, de moment redirigeix a ErrorPage

--- General/footer.php
+++ General/footer.php
@@ -5,6 +5,33 @@
         <input type="button" value="-->" id="btn2" onClick="endevant()">
     </div>
     <div id="about">
+        <table>
+            <tr id="FilaApartat">
+                <td class="Apartat"><a href="../ErrorPage/error.php">Avís Legal</a></td>
+                <td class="Apartat">Sobre Nosaltres</td>
+                <td class="Apartat">Contacte</td>
+                <td class="Apartat">Horaris</td>
+                <td class="Apartat">Ubicació</td>
+            </tr>
+            <tr id="FilaContingut">
+                <td class="Contingut"></td>
+                <td class="Contingut"><p>Hola sóm la cantina<br>
+                                        del Institut Pedralbes<br> 
+                                        desde 1849 i sóm del <br> 
+                                        Barça.</p></td>
+
+                <td class="Contingut"><p>Correu: <br>
+                                     Rubén: a18rubonclop@inspedralbes.cat <br>
+                                      Ángel: a18anggarvic@gmail.com <br>
+                                       Àlex:a18aleperfer@inspedralbes.cat</p></td>
+
+                <td class="Contingut"><p>De Dilluns a Divendres <br>
+                                        7:00-19:00</p></td>
+
+                <td class="Contingut"><p>Av. Esplugues, 36-42. <br>
+                                        08034 Barcelona</p></td>
+            </tr>
+        </table>
         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique eveniet nam blanditiis maiores quaerat nesciunt dicta distinctio quasi quas. Aliquam eius animi alias recusandae dicta eveniet? Numquam culpa est voluptatibus?</p>
     </div>
 </div>
