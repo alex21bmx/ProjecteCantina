2363d35ab23c0dce9192ada08c53013f98c53176
SnowMan3Sixty <a18rubonclop@inspedralbes.cat>
2020-11-15 01:07:40 +0100

    menu generado a través de servidor

--- js/final.js
+++ js/final.js
@@ -1,11 +1,24 @@
 function imprimeixComanda(){
     let json = JSON.parse(localStorage.getItem("comanda"));
     let cadena = "";
+    let preuTotal;
     cadena+='<h3 id="h3Final"><b>Productes</b></h3>';
     cadena+='<ul id="ulFinal">';
+
+    let inp = file_get_contents('./admin/menu.json');
+    let tempArray = json_decode(inp);
+
     for (let key in json["productes"]){
         var attrName = key;
         cadena+=("<li><t>"+key+" - "+json["productes"][key]+" unitats </li>");
+/*
+        for (let i = 0; i < sizeof(tempArray); i++) {           
+            if(tempArray[i]->nom == key){
+                preuTotal += ((json["productes"][key]) * tempArray[i]->preu);
+                console.log(preuTotal);
+            }
+        }
+ */       
     }
     cadena+="</ul>"
     cadena+=('<p id="pFinal"><b>Preu </b>'+json["preu"]+'€</p>');
--- menu.php
+++ menu.php
@@ -40,13 +40,20 @@
         $inp = file_get_contents('./admin/menu.json');
         $tempArray = json_decode($inp);
         
+        echo('<div class="titolMenu" >');
+        if($menu == 'pati'){
+          echo('Carta Pati');
+        }else{
+          echo('Carta Migdia');
+        }
         
+        echo('</div>');
         echo ('
+          <br><br>
           <div id="graella">
           
-          ');       
+          ');     
 
-        
         for ($i=0; $i < sizeof($tempArray); $i++) { 
           if($tempArray[$i]->torn == $menu){          
             echo('
@@ -54,7 +61,7 @@
                 <img class = "menuImg" src = "' . $tempArray[$i]->rutaMenu . '">
                 <div>
                   <text class="nombre">'. $tempArray[$i]->nom . '</text>        
-                  <text class="precio">'.$tempArray[$i]->preu.'</text>
+                  <text class="precio">'.$tempArray[$i]->preu.'€</text>
                   </br>
                   <button class="botonMenos">-</button>
                   <text class = "cantidad">0</text>
@@ -65,238 +72,10 @@
           }                   
         }
         
-        echo('</div>');
-          
-        
+        echo('</div>');  
        
       ?>
-      <!--
-        <div id = "pati">
-          <table class = "tablaMenu">
-            <tr>
-              <th class="titolMenu" colspan = "3">Carta Pati</th>
-            </tr>
-            <tr>
-              <td class="tdMenu">
-                <img class = "menuImg" src = "./src/img/menu_bocadillo_fuet.jpg">
-                <div>
-                  <text class="nombre">Bocadillo Fuet</text>        
-                  <text class="precio">1.70€</text>
-                  </br>
-                  <button class="botonMenos">-</button>
-                  <text class = "cantidad">0</text>
-                  <button class="botonMas">+</button>          
-                </div>
-              </td>
-              <td class="tdMenu">
-                <img class = "menuImg" src = "./src/img/menu_bocadillo_queso.jpg">
-                <div>
-                  <text class="nombre">Bocadillo Queso</text>        
-                  <text class="precio">1.70€</text>
-                  </br>
-                  <button class="botonMenos">-</button>
-                  <text class = "cantidad">0</text>
-                  <button class="botonMas">+</button>          
-                </div>
-              </td>
-              <td class="tdMenu">
-                <img class = "menuImg" src = "./src/img/menu_bocadillo_chistorra.jpg">
-                <div>
-                  <text class="nombre">Bocadillo Chistorra</text>        
-                  <text class="precio">2.50€</text>
-                  </br>
-                  <button class="botonMenos">-</button>
-                  <text class = "cantidad">0</text>
-                  <button class="botonMas">+</button>        
-                </div>  
-              </td>
-            </tr>
-            <tr>
-              <td class="tdMenu">
-                <img class = "menuImg" src = "./src/img/esmorzar_torrada.jpg">
-                <div>
-                <text class="nombre">Torrades</text>         
-                  <text class="precio">0.90€</text>
-                  </br>
-                  <button class="botonMenos">-</button>
-                  <text class = "cantidad">0</text>
-                  <button class="botonMas">+</button>          
-                </div>
-              </td>
-              <td class="tdMenu">
-                <img class = "menuImg" src = "./src/img/esmorzar_pancakes.jpg">
-                <div>
-                  <text class="nombre">Pancakes</text>                      
-                  <text class="precio">1.10€</text>
-                  </br>
-                  <button class="botonMenos">-</button>
-                  <text class = "cantidad">0</text>
-                  <button class="botonMas">+</button>          
-                </div>
-              </td>
-              <td class="tdMenu">
-                <img class = "menuImg" src = "./src/img/esmorzar_donutxoco.jpg">
-                <div>
-                  <text class="nombre">Donut</text>        
-                  <text class="precio">0.90€</text>
-                  </br>
-                  <button class="botonMenos">-</button>
-                  <text class = "cantidad">0</text>
-                  <button class="botonMas">+</button>        
-                </div>  
-              </td>
-            </tr>
-            <tr>
-              <td class="tdMenu">
-                <img class = "menuImg" src = "./src/img/esmorzar_cafe.jpg">
-                <div>
-                  <text class="nombre">Café</text>         
-                  <text class="precio">1.70€</text>
-                  </br>
-                  <button class="botonMenos">-</button>
-                  <text class = "cantidad">0</text>
-                  <button class="botonMas">+</button>          
-                </div>
-              </td>
-              <td class="tdMenu">
-                <img class = "menuImg" src = "./src/img/esmorzar_te.jpg">
-                <div>
-                  <text class="nombre">Te</text>         
-                  <text class="precio">1.70€</text>
-                  </br>
-                  <button class="botonMenos">-</button>
-                  <text class = "cantidad">0</text>
-                  <button class="botonMas">+</button>          
-                </div>
-              </td>
-              <td class="tdMenu">
-                <img class = "menuImg" src = "./src/img/esmorzar_naranja.jpg">
-                <div>
-                  <text class="nombre">Suc de taronja</text>        
-                  <text class="precio">1.80€</text>
-                  </br>
-                  <button class="botonMenos">-</button>
-                  <text class = "cantidad">0</text>
-                  <button class="botonMas">+</button>          
-                </div>
-              </td>
-            </tr>
-          </table> 
-        </div>
-        <div id = "migdia">
-          <table class = "tablaMenu">
-            <tr>
-              <th class="titolMenu" colspan = "3">Carta Migdia</th>
-            </tr>
-            <tr>
-              <td class="tdMenu"> 
-                <img class = "menuImg" src = "./src/img/menu_ensalada.jpg">
-                <div>
-                  <text class="nombre">Amanida</text>          
-                  <text class="precio">2.70€</text>
-                  </br>
-                  <button class="botonMenos">-</button>
-                  <text class = "cantidad">0</text>
-                  <button class="botonMas">+</button>          
-                </div>
-              </td>
-              <td class="tdMenu">
-                <img class = "menuImg" src = "./src/img/menu_entrecot.jpg">
-                <div>
-                  <text class="nombre">Entrecot</text>          
-                  <text class="precio">11.70€</text>
-                  </br>
-                  <button class="botonMenos">-</button>
-                  <text class = "cantidad">0</text>
-                  <button class="botonMas">+</button>          
-                </div>
-              </td>
-              <td class="tdMenu">
-                <img class = "menuImg" src = "./src/img/menu_natillas.jpg">
-                <div>
-                  <text class="nombre">Natillas</text>                            
-                  <text class="precio">1.50€</text>
-                  </br>
-                  <button class="botonMenos">-</button>
-                  <text class = "cantidad">0</text>
-                  <button class="botonMas">+</button>        
-                </div>  
-              </td>
-            </tr>
-            <tr>
-              <td class="tdMenu">
-                <img class = "menuImg" src = "./src/img/menu_tallarines.jpg">
-                <div>
-                  <text class="nombre">Tallarines</text>          
-                  <text class="precio">7.50€</text>
-                  </br>
-                  <button class="botonMenos">-</button>
-                  <text class = "cantidad">0</text>
-                  <button class="botonMas">+</button>          
-                </div>
-              </td>         
-              <td class="tdMenu">
-                <img class = "menuImg" src = "./src/img/menu_sushi.jpg">
-                <div>
-                  <text class="nombre">Sushi</text>          
-                  <text class="precio">12.80€</text>
-                  </br>
-                  <button class="botonMenos">-</button>
-                  <text class = "cantidad">0</text>
-                  <button class="botonMas">+</button>          
-                </div>
-              </td>
-              <td class="tdMenu">
-                <img class = "menuImg" src = "./src/img/menu_tiramisú.jpg">
-                <div>
-                  <text class="nombre">Tiramisú</text>          
-                  <text class="precio">4.50€</text>
-                  </br>
-                  <button class="botonMenos">-</button>
-                  <text class = "cantidad">0</text>
-                  <button class="botonMas">+</button>        
-                </div>  
-              </td>
-            </tr>
-            <tr>
-            <td class="tdMenu">
-                <img class = "menuImg" src = "./src/img/menu_leon.jpg">
-                <div>
-                  <text class="nombre">León come gambas</text>          
-                  <text class="precio">7.50€</text>
-                  </br>
-                  <button class="botonMenos">-</button>
-                  <text class = "cantidad">0</text>
-                  <button class="botonMas">+</button>          
-                </div>
-              </td>
-              <td class="tdMenu">
-                <img class = "menuImg" src = "./src/img/menu_trucha.jpg">
-                <div>
-                  <text class="nombre">Truita de granja</text>         
-                  <text class="precio">7.90€</text>
-                  </br>
-                  <button class="botonMenos">-</button>
-                  <text class = "cantidad">0</text>
-                  <button class="botonMas">+</button>          
-                </div>
-              </td>
-              <td class="tdMenu">
-                <img class = "menuImg" src = "./src/img/menu_fruta.jpg">
-                <div>
-                  <text class="nombre">Fruita</text>        
-                  <text class="precio">0.65€</text>
-                  </br>
-                  <button class="botonMenos">-</button>
-                  <text class = "cantidad">0</text>
-                  <button class="botonMas">+</button>        
-                </div>  
-              </td>            
-            </tr>
-          </table>
-        </div>
-      </div>
-        -->
+      
     </div>   
     <footer id="footer">
         <?php require_once("imports/footer.php"); ?>
